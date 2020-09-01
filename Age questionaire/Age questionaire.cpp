b478f523d315aa78bea57b210ac76c88db819dff
omakaroni <jon_resell_3@hotmail.com>
2020-09-01 15:15:07 +0200

    added line

--- Age questionaire/Age questionaire.cpp
+++ Age questionaire/Age questionaire.cpp
@@ -2,19 +2,19 @@
 	int birthyear;
 	int currentyear;
 	int age;
-int main()
-{
-	std::cout << "Enter your birth year:\n";
-	std::cin >> birthyear;
-	std::cout << "Enter current year:\n";
-	std::cin >> currentyear;
-	age = currentyear - birthyear;
-	std::cout << "Your current age is :"<< age <<std::endl;
-if (age < 20) {
-	std::cout << "You are a teenager";
-}
-else{
-	std::cout << "You are a boomer";
-}
-
-}
+	int main()
+	{
+		std::cout << "Enter your birth year:\n";
+		std::cin >> birthyear;
+		std::cout << "Enter current year:\n";
+		std::cin >> currentyear;
+		age = currentyear - birthyear;
+		std::cout << "Your current age is :" << age << std::endl;
+		if (age < 20) {
+			std::cout << "You are a teenager";
+		}
+		else {
+			std::cout << "You are a boomer";
+		}
+		return 0;
+	}
