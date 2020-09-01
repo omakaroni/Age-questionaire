b7bde1e9c0074e88d5c9a5f09ae00ac7bca0fc95
omakaroni <jon_resell_3@hotmail.com>
2020-09-01 14:43:59 +0200

    Add project files.

---
+++ Age questionaire/Age questionaire.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+	int birthyear;
+	int currentyear;
+	int age;
+int main()
+{
+	std::cout << "Enter your birth year:\n";
+	std::cin >> birthyear;
+	std::cout << "Enter current year:\n";
+	std::cin >> currentyear;
+	age = currentyear - birthyear;
+	std::cout << "Your Age is :"<< age <<std::endl;
+if (age < 20) {
+	std::cout << "you are teenager";
+}
+else{
+	std::cout << "you are old";
+}
+
+}
